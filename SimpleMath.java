0f1663b1012ce34d0bc548425dedbd7157f58b04
emturn02 <ethanturner0401@gmail.com>
2025-01-13 15:44:52 -0500

    Hello World!

---
+++ SimpleMath.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SimpleMath
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int a = scan.nextInt();
+        System.out.println("Enter your second number: ");
+        int b = scan.nextInt();
+        int addResult = addition(a, b);
+        int subResult = subtraction(a, b);
+        System.out.println(a + " + " + b + " = " + addResult);
+        System.out.println(a + " - " + b + " = " + subResult);
+    }
+
+    private static int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    private static int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+}
