baa183cc7eb902d63f50e21870958f389b010312
emturn02 <ethanturner0401@gmail.com>
2025-01-15 12:10:29 -0500

    Simple calculator with a few functions.

---
+++ Calculator/Calculator.java
@@ -0,0 +1,54 @@
+package Calculator;
+
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    private static double addition(double a, double b)
+    {
+        return a + b;
+    }
+
+    private static int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    private static double subtraction(double a, double b)
+    {
+        return a - b;
+    }
+
+    private static int multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    private static double multiplication(double a, double b)
+    {
+        return a * b;
+    }
+
+    private static int division(int a, int b)
+    {
+        return a / b;
+    }
+
+    private static double division(double a, double b)
+    {
+        return a / b;
+    }
+
+    private static int modulus(int a, int b)
+    {
+        return a % b;
+    }
+}
