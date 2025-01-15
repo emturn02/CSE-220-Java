bfee6bcf3983c016149d2e8b52f92fca6bb6a255
emturn02 <ethanturner0401@gmail.com>
2025-01-15 12:26:30 -0500

    Simple calculator with a few functions.

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,10 +1,46 @@
 package Calculator;
 
+import java.util.Scanner;
+
 public class Calculator
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+        int userChoice;
+        int userResult;
+        int firstUserNum;
+        int secUserNum;
 
+        System.out.println("\t\t\t\t\t Calculator");
+        System.out.println("\t\t\t\t\t[0] Addition");
+        System.out.println("\t\t\t\t\t[1] Subtraction");
+        System.out.println("\t\t\t\t\t[2] Multiplication");
+        System.out.println("\t\t\t\t\t[3] Division");
+        System.out.println("\t\t\t\t\t[4] Modulus");
+
+        System.out.println("Choose an option: ");
+        userChoice = scanner.nextInt();
+
+        switch(userChoice)
+        {
+            case 0:
+                System.out.println("\nEnter the first number: ");
+                firstUserNum = scanner.nextInt();
+                System.out.println("Enter the second number: ");
+                secUserNum = scanner.nextInt();
+                userResult = addition(firstUserNum, secUserNum);
+                System.out.println(firstUserNum + " + " + secUserNum + " = " + userResult);
+                break;
+            case 1:
+                System.out.println("\nEnter the first number: ");
+                firstUserNum = scanner.nextInt();
+                System.out.println("Enter the second number: ");
+                secUserNum = scanner.nextInt();
+                userResult = subtraction(firstUserNum, secUserNum);
+                System.out.println(firstUserNum + " - " + secUserNum + " = " + userResult);
+                break;
+        }
     }
 
     private static int addition(int a, int b)
